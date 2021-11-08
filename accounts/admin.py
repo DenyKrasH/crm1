cae6cf29f8f2a8bab7a35299479aa52f6816d6df
Ekleft_Sharovskiy <Denis.Sharovskiy@mvideo.ru>
2021-11-08 20:57:10 +0300

    add models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
+from accounts.models import Customer, Product, Order, Tag
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Tag)
