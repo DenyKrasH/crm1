a278b6330e7433ace54fff72fd1c846e22140756
Ekleft_Sharovskiy <Denis.Sharovskiy@mvideo.ru>
2021-11-09 22:47:30 +0300

    add CRUD models and add data on page

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from accounts.models import Order
+from django.forms import ModelForm
+
+
+class OrderForm(ModelForm):
+    class Meta:
+        model = Order
+        fields = '__all__'
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,11 @@
-from accounts.views import home, products, customer
+from accounts.views import *
 from django.urls import path
 
 urlpatterns = [
-    path('', home),
-    path('products/', products),
-    path('customer', customer)
+    path('', home, name='home'),
+    path('products/', products, name='products'),
+    path('customer/<int:pk>', customer, name='customer'),
+    path('create_order', createOrder, name='create_order'),
+    path('update_order/<int:pk>', updateOrder, name='update_order'),
+    path('delete_order/<int:pk>', deleteOrder, name='delete_order'),
 ]
